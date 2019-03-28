2f1ceb96b43c3c39375ed62ed8b5c5bc42e0a49e
lmh <2297181256@qq.com>
2019-03-28 14:57:54 +0800

    设计师团页面改版

--- resources/views/home/designer/index.blade.php
+++ resources/views/home/designer/index.blade.php
@@ -17,96 +17,35 @@
     </div>
 
     <div>
-        <ul class="product-show-ul">
-            <?php $i = 0; ?>
-            @foreach($oDesigners as $designer)
-                <?php $i++; ?>
-                @if($i % 2 == 1)
-                    <li>
-                        <div class="product-content">
-                            <div class="left am-u-sm-12 am-u-md-6 am-u-lg-6 product-content-left">
-                                <div class="product-show-title">
-                                    <h3>{{$designer->name}}</h3>
-                                    @if($designer->sex == 1)
-                                        <span class="am-icon-mars am-icon-sm"></span>
-                                    @else
-                                        <span class="am-icon-venus am-icon-sm"></span>
-                                    @endif
-                                    <span>{{$designer->title}}</span>
-                                </div>
-
-                                <div class="product-show-content">
-                                    <div class="product-add">
-                                        <span>人气指数：</span>
-                                        <div>
+        <div class="container">
+            <!--about-container start-->
+            <div class="about-container">
+                <div class="our-team">
+                    <div class="am-g">
+                        @foreach($oDesigners as $designer)
+                            <div class="am-u-md-3">
+                                <div class="team-box">
+                                    <div class="our-team-img">
+                                        <img src="{{$designer->photo}}" alt=""/>
+                                    </div>
+                                    <div class="team_member--body">
+                                        <h3 class="team_member--name">{{$designer->name}}</h3>
+                                        <span class="team_member--position" style="color: red;">{{$designer->title}}</span>
+                                        <span class="team_member--email">
+											<a href="">{{$designer->introduction}}</a>
+										</span>
+                                        <ul class="team_member--links">
                                             @for($j = 0; $j < $designer->stars; $j++)
-                                                <span class="am-icon-star am-icon-sm"></span>
+                                                <li><span class="am-icon-star am-icon-sm"></span></li>
                                             @endfor
-                                        </div>
-                                        <i class="am-icon-dribbble"></i>
-                                    </div>
-                                    <div class="product-intro">
-                                        <span>个人简介：</span>
-                                        <div>
-                                            <p>
-                                                {{$designer->introduction}}
-                                            </p>
-                                        </div>
-                                        <i class="am-icon-tasks"></i>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
-                            <div class="right am-u-sm-12 am-u-md-6 am-u-lg-6 product-content-right">
-                                <img class="product-img" src="{{$designer->photo}}"/>
-                            </div>
-                            <div class="clear"></div>
-                        </div>
-                    </li>
-                @else
-                    <li class="gray-li">
-                        <div class="product-content">
-                            <div class="left am-u-sm-12 am-u-md-6 am-u-lg-6 product-content-left">
-                                <img class="product-img" src="{{$designer->photo}}"/>
-
-                            </div>
-                            <div class="right am-u-sm-12 am-u-md-6 am-u-lg-6 product-content-right">
-                                <div class="product-show-title">
-                                    <h3>{{$designer->name}}</h3>
-                                    @if($designer->sex == 1)
-                                        <span class="am-icon-mars am-icon-sm"></span>
-                                    @else
-                                        <span class="am-icon-venus am-icon-sm"></span>
-                                    @endif
-                                    <span>{{$designer->title}}</span>
-                                </div>
-
-                                <div class="product-show-content">
-                                    <div class="product-add">
-                                        <span>人气指数：</span>
-                                        <div>
-                                            @for($j = 0; $j < $designer->stars; $j++)
-                                                <span class="am-icon-star am-icon-sm"></span>
-                                            @endfor
-                                        </div>
-                                        <i class="am-icon-dribbble"></i>
-                                    </div>
-                                    <div class="product-intro">
-                                        <span>个人简介：</span>
-                                        <div>
-                                            <p>
-                                                {{$designer->introduction}}
-                                            </p>
-                                        </div>
-                                        <i class="am-icon-tasks"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="clear"></div>
-                        </div>
-                    </li>
-                @endif
-            @endforeach
-            <div class="clear"></div>
-        </ul>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
